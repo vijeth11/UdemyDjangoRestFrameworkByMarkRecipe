c2911b95838703d167b6d82c02c32f253242541a
vijeth11 <vijeth11@yahoo.co.in>
2020-12-20 10:30:53 -0500

    added  Tag urls and test cases

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email='test@test.com', password='123456'):
+    """Create a sampleUser"""
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -34,3 +40,11 @@ class ModelTests(TestCase):
             password=password)
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+        self.assertEqual(str(tag), tag.name)
